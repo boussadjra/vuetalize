b693d0a9b28b17a8f8eb34c29e8ba9273265939e
boussadjra <cb_boussadjra@esi.dz>
2022-11-18 10:45:54 +0100

    remove vitessg

--- src/main.ts
+++ src/main.ts
@@ -14,12 +14,14 @@ console.log("generatedRoutes", generatedRoutes);
 console.log("routes", routes);
 
 console.log("--------------------");
-export const createApp = ViteSSG(
-  App,
-  { routes: generatedRoutes, base: import.meta.env.BASE_URL },
-
-  ({ app, router, routes, isClient, initialState }) => {
-    app.use(createPinia());
-    app.use(vuetify);
-  }
+const app = createApp(App);
+app.use(createPinia());
+app.use(vuetify);
+app.use(createHead());
+app.use(
+  createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: generatedRoutes,
+  })
 );
+app.mount("#app");
