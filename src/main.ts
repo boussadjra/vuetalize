01d49b4dfd81784eeb93b319e4b773b984e55d53
boussadjra <cb_boussadjra@esi.dz>
2022-11-22 07:28:31 +0100

    fix auto imported pages

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,14 @@ import i18n from "./plugins/i18n";
 import App from "./App.vue";
 import { createRouter, createWebHistory } from "vue-router";
 import { setupLayouts } from "virtual:generated-layouts";
-import generatedRoutes from "virtual:generated-pages";
+import generatedRoutes from "~pages";
 
 import vuetify from "./plugins/vuetify";
 import "./assets/main.css";
 import { createHead } from "@vueuse/head";
 
 const routes = setupLayouts(generatedRoutes);
-console.log("--------------------");
-console.log("generatedRoutes", generatedRoutes);
-console.log("routes", routes);
 
-console.log("--------------------");
 const app = createApp(App);
 app.use(createPinia());
 app.use(vuetify);
